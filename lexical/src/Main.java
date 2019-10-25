404d43c5310addaa6ccdcbc6ee0b90acba27b1a6
Stefan Ghioci <ghioci.stefan@gmail.com>
2019-10-25 12:14:56 +0300

    Add console menu to choose between 3 problems to solve

--- lexical/src/Main.java
+++ lexical/src/Main.java
@@ -1,32 +1,65 @@
 import algorithm.Analyser;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main
 {
 
-    public static void main(String[] args)
+    public static void main(String[] args) throws IOException
     {
-        File input = new File(args[0]);
+        File input = null;
         Analyser analyser = null;
-        analyser = new Analyser(input);
 
-        try
+        while (true)
         {
-            analyser.analyse();
+            System.out.println();
+            System.out.println("Choose a problem:");
+            System.out.println("1. Perimeter and area");
+            System.out.println("2. Sum of n given numbers");
+            System.out.println("3. Greatest Common Divisor");
+            System.out.println("x. Exit");
 
-            System.out.println("------PROGRAM INTERNAL FORM------");
-            analyser.getProgramInternalForm().forEach(System.out::println);
+
+            BufferedReader reader =
+                    new BufferedReader(new InputStreamReader(System.in));
+
+            String problem = reader.readLine();
+            switch (problem)
+            {
+                case "1":
+                    input = new File("resources/per_area/");
+                    break;
+                case "2":
+                    input = new File("resources/nsum/");
+                    break;
+                case "3":
+                    input = new File("resources/gcd/");
+                    break;
+                case "x":
+                    System.exit(0);
+                default:
+                    continue;
+            }
+            try
+            {
+                analyser = new Analyser(input);
+                analyser.analyse();
+
+                System.out.println("------PROGRAM INTERNAL FORM------");
+                analyser.getProgramInternalForm().forEach(System.out::println);
 
 
-            System.out.println("----------SYMBOLS TABLE----------");
-            analyser.getSymbols().printInOrder();
+                System.out.println("----------SYMBOLS TABLE----------");
+                analyser.getSymbols().printInOrder();
 
+            }
+            catch (Exception e)
+            {
+                System.out.println("ERROR:" + e.getMessage());
+            }
         }
-        catch (Exception e)
-        {
-            System.out.println("ERROR:" + e.getMessage());
-        }
-
     }
 }
